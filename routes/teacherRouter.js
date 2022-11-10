325fbda209db91293552c6228f44de81cd530ec2
meezan-mallick <meezan.mallick@gmail.com>
2022-11-10 22:02:36 +0530

    update endpoint added

--- routes/teacherRouter.js
+++ routes/teacherRouter.js
@@ -4,15 +4,17 @@ let router = express.Router();
 
 router.post("/", async (req, res) => {
   const teacher = req.body;
-  const isExists = await teacherModel.findOne({email: teacher.email}); 
-  if(isExists) return res.status(200).send({ message: "Teacher Already Exists.." });
+  const isExists = await teacherModel.findOne({ email: teacher.email });
+  if (isExists)
+    return res.status(200).send({ message: "Teacher Already Exists.." });
   const newTeacher = new teacherModel({
     name: teacher.name,
     email: teacher.email,
     mobile: teacher.mobile,
     gender: teacher.gender,
   });
-  newTeacher.save()
+  newTeacher
+    .save()
     .then((data) => {
       console.log("DATA SAVED : ", data);
       return res.status(200).send({ message: "Teacher Data Saved.." });
@@ -23,17 +25,16 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  teacherModel.find()
-      .then((data) => {
-          if (data.length > 0) return res.send(data);
-          else return res.send({ message: 'No Data Found' });
-      })
-      .catch((e) => {
-          return res.send({ message: e });
-      });
+  teacherModel
+    .find()
+    .then((data) => {
+      if (data.length > 0) return res.send(data);
+      else return res.send({ message: "No Data Found" });
+    })
+    .catch((e) => {
+      return res.send({ message: e });
+    });
 });
-
-
 
 //Delete Student by Id
 router.get("/delete/:id", async (req, res) => {
@@ -41,22 +42,54 @@ router.get("/delete/:id", async (req, res) => {
   const student = await teacherModel.findById(id);
   console.log(student);
   if (student) {
-      teacherModel.deleteOne(student)
-          .then(() => {
-              return res.send({ message: "Student Deleted" });
-          });
+    teacherModel.deleteOne(student).then(() => {
+      return res.send({ message: "Student Deleted" });
+    });
   } else {
-      return res.send({ message: `Data Not Found For ID: ${id}` })
+    return res.send({ message: `Data Not Found For ID: ${id}` });
   }
 });
 
-router.get('/:id', async(req,res)=>{
+//Get the user
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const student = await teacherModel.findById(id);
-  if(student) {
-    console.log('rocord found');
+  if (student) {
+    console.log("rocord found");
     console.log(student);
+    return res.send({ message: `Data Found For ID: ${id}` });
+  } else {
+    console.log("data not found");
+    return res.send({ message: `Data Not Found For ID: ${id}` });
   }
-})
+});
+
+//UPDATE
+router.post("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const teacher = req.body; //get updated data from req body
+
+  updatedData = {
+    name: "tesr",
+    gender: "male",
+    email: "mallick1233@gmail.com",
+    mobile: "92839172039",
+  };
+
+  const student = await teacherModel.findOneAndUpdate(
+    { _id: id },
+    updatedData,
+    { new: true }
+  );
+
+  if (student) {
+    console.log("rocord found");
+    console.log(student);
+    return res.send({ message: `Data Found For ID: ${id}` });
+  } else {
+    console.log("data not found");
+    return res.send({ message: `Data Not Found For ID: ${id}` });
+  }
+});
 
 module.exports = router;
